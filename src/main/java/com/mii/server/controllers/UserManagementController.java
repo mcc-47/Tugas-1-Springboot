17bfa6f6ceee199e7783ff8ac5f9f4e6ee5f777c
christinaregita <80805524+christinaregita@users.noreply.github.com>
2021-03-30 10:25:25 +0700

    tugas springboot 1 regita
    tugas springboot 1 regita

---
+++ src/main/java/com/mii/server/controllers/UserManagementController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mii.server.controllers;
+
+import com.mii.server.dto.UserManagementDTO;
+import com.mii.server.entities.Addresses;
+import com.mii.server.entities.Contacts;
+import com.mii.server.entities.Educations;
+import com.mii.server.entities.Employees;
+import com.mii.server.entities.Majors;
+import com.mii.server.entities.Universities;
+import com.mii.server.entities.Villages;
+import com.mii.server.repositories.EmployeeRepository;
+import com.mii.server.services.NotificationService;
+import javax.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Asus
+ */
+@RestController
+@RequestMapping("/api/email-demo")
+public class UserManagementController {
+    
+    @Autowired
+    NotificationService notificationService;
+    
+    @PostMapping()
+    public ResponseEntity<?> sendEmailDemo (Integer employeeId) throws MessagingException{
+        if (employeeId == null){
+            return new ResponseEntity("Please insert employee ID", HttpStatus.NOT_ACCEPTABLE);
+        }
+        notificationService.sendEmail(employeeId);
+        return new ResponseEntity(true, HttpStatus.ACCEPTED);
+
+    }
+
+    @PostMapping("/register")
+    public Employees saveEmployee(UserManagementDTO userManagementDTO){
+        Employees employee = new Employees(
+            userManagementDTO.getPrefix(),
+            userManagementDTO.getEmployeeId(),
+            userManagementDTO.getEmployeeName(),
+            userManagementDTO.getBirthDate(),
+            userManagementDTO.getGender(),
+            userManagementDTO.getEmail(),
+            new Addresses (userManagementDTO.getPrefix(),userManagementDTO.getEmployeeId(),
+                new Villages(userManagementDTO.getVillageId())),
+            new Educations(userManagementDTO.getPrefix(),userManagementDTO.getEmployeeId()),
+                new Majors(userManagementDTO.getMajorId()), new Universities(userManagementDTO.getUniversityId()),
+            new Contacts(userManagementDTO.getPrefix(),userManagementDTO.getEmployeeId(),userManagementDTO.getPhone(),
+                userManagementDTO.getLinkedin())
+        );
+        EmployeeRepositoryRepository.save(employee);
+        return employee;
+    } 
+}
