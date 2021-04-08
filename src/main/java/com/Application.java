840aecafac5b05d449c83d25239a1ef06c6bc85d
Jaka Brajadenta <jakabrajadenta@gmail.com>
2021-04-08 10:18:51 +0700

    rebuild local project

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,7 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//SEMANGAT REBUILD UDAH OKE
 @SpringBootApplication
 public class Application {
 
